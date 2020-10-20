a3394e10c4d6dc17569604bfa0a147f24a47ceac
GattaFurn <72799595+GattaFurn@users.noreply.github.com>
2020-10-20 15:16:16 +0200

    oui

--- connection/views.py
+++ connection/views.py
@@ -2,21 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 
-class ConnectionForm (forms.Form):
-    username = forms.CharField(label = "Username")
-    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+#iterables
+COLOR_CHOICES = (
+    ("BL", "Blue"),
+    ("PK", "Pink"),
+    ('YW', 'Yellow'),
+    ('GN', 'Green'),
+    ('VT', 'Violet'),
+    ('RD', 'Red'),
+    ('OE', 'Orange'),
+    ('CN', 'Cyan'),
+)
 
+class ConnectionFormPlayer (forms.Form):
+    username = forms.CharField(label = "Username", max_length=50)
+    password = forms.CharField(label="Password", max_length=20, widget=forms.PasswordInput())
 
+class ConnectionFormNewPlayer(forms.Form):
+    username = forms.CharField(label = "Username", max_length=50)
+    password = forms.CharField(label="Password", max_length=20, widget=forms.PasswordInput())
+    colorchoice = forms.ChoiceField(label="Your color", choices=COLOR_CHOICES)
 
 def index(request):
     if request.method == "GET": # get connection page
-        form = ConnectionForm() # empty form
-        return render(request, "connection/index.html", { "form": form })
+        formPlayer = ConnectionFormPlayer() # empty form
+        formNewPlayer = ConnectionFormNewPlayer()
+        return render(request, "connection/index.html", { "formPlayer": formPlayer , "formNewPlayer": formNewPlayer})
 
     if request.method == "POST": # post a connection
-        form = ConnectionForm(request.POST) #auto fill form with info in POST
+        formPlayer = ConnectionFormPlayer(request.POST) #auto fill form with info in POST
         
-        if form.is_valid():
+        if formPlayer.is_valid():
             # Check credentials
             return HttpResponse("OK")
         return HttpResponse("KO")
--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #décide de la page a renvoyer via les views
     path('move', views.apply_move)
 ]
