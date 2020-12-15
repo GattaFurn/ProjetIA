517fe05f93679a140b1a320fffe25ed5c6b6b750
LiocansG <61200583+LiocansG@users.noreply.github.com>
2020-12-15 19:29:13 +0100

    correction bug training

--- game/training.py
+++ game/training.py
@@ -3,8 +3,9 @@ from game.ia import index,actions
 import random
 import json
 import time
+from django.shortcuts import redirect
 
-NB_ITERATION = 20
+NB_ITERATION = 5
 
 def training(request):
     eps = 0
@@ -12,17 +13,21 @@ def training(request):
     game_state = reset(eps)
     for i in range(NB_ITERATION):
         while(game_state["code"]==0):
-            move = random_play(game_state)
-            game_state = apply_move(game_state,move)
-            game_state = game_is_win(game_state)
-            game_state["players"][0]["box_taken"],game_state["board"] = zone_search(game_state["board"],0,game_state["players"][0]["position"])
-            game_state = switch_player(game_state)
-            game_state = index(game_state)
+            if(game_state["current_player"] == 0):
+                move = random_play(game_state)
+                game_state = apply_move(game_state,move)
+                game_state = game_is_win(game_state)
+                game_state["players"][0]["box_taken"],game_state["board"] = zone_search(game_state["board"],0,game_state["players"][0]["position"])
+                game_state = switch_player(game_state)
+            else:
+                game_state = index(game_state)
+            affichage_plateau(game_state)
         print("fini")
         if(eps != 1):
             eps += step
         game_state = reset(eps)
     print("Totalement fini")
+    return redirect('../connection')
         
 
 def reset(eps):
@@ -39,13 +44,32 @@ def reset(eps):
 
 def random_play(game_state):
     action = random.randint(0,3)
-    move = game_state["players"][0]["position"]
+    move = game_state["players"][0]["position"].copy()
     move[0] += actions[action][0]
     move[1] += actions[action][1]
     while (not correct_move(game_state,move)):
         action = random.randint(0,3)
-        move = game_state["players"][0]["position"]
+        move = game_state["players"][0]["position"].copy()
         move[0] += actions[action][0]
         move[1] += actions[action][1]
     return move
-    
+
+def affichage_plateau(game_state):
+    """
+        Show the grid
+    """
+    print("---------------------")
+    ligne = 0
+    for line in game_state["board"]:
+        colonne = 0
+        print("[",end="")
+        for n in line:
+            if((game_state["players"][0]["position"] == [ligne,colonne])):
+                print("X ,",end="")
+            if((game_state["players"][1]["position"] == [ligne,colonne])):
+                print("O ,",end="")
+            if(game_state["players"][1]["position"] != [ligne,colonne]) and (game_state["players"][0]["position"] != [ligne,colonne]):
+                print(n,",",end="")
+            colonne += 1
+        print("]")
+        ligne +=1
